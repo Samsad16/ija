597d742a6cb28a06ed4fb27f78138aaed15465f1
nettle <152798336+Samsad16@users.noreply.github.com>
2026-04-11 11:29:04 +0200

    Add classes Game and GameFactory

--- src/ija/ija2025/homework2/game.java
+++ src/ija/ija2025/homework2/game.java
@@ -1,5 +1,21 @@
 package ija.ija2025.homework2;
 
-public class game {
-    
+public class Game {
+    public void addObserver(GameObserver go) {
+        
+    }
+
+    public Unit createUnit(String[] str1, String[] str2, int i, int j) {
+
+    }
+
+    static void moveUnit(Position unitsoldposition, Position newposition) {
+
+    }
+}
+
+public class GameFactory {
+    static Game createGame(String[] str) {
+        return new Game();
+    }
 }
