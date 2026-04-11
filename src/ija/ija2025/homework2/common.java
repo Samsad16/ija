c99cdb927420381138883a9598ce12b96cf8f44a
nettle <152798336+Samsad16@users.noreply.github.com>
2026-04-11 11:27:03 +0200

    Update common.java

--- src/ija/ija2025/homework2/common.java
+++ src/ija/ija2025/homework2/common.java
@@ -1,5 +1,17 @@
 package ija.ija2025.homework2;
 
-public class common {
+public class Position {
     
+}
+
+public class GameEvent {
+
+}
+
+public class Unit {
+    private Position position;
+
+    public Position getPosition() {
+        return position;
+    }
 }
