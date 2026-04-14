b1bc9efeb57ee68a63c24c1e7b014919fc4c6377
Tereza Dolakova <xdolakt00@stud.fit.vut.cz>
2026-04-14 14:18:24 +0200

    Implemented gameevent

--- src/ija/ija2025/homework2/common/GameEvent.java
+++ src/ija/ija2025/homework2/common/GameEvent.java
@@ -1,5 +1,24 @@
 package ija.ija2025.homework2.common;
 
+// Event describing succesful unit moveement in the game
 public class GameEvent {
+    // Original unit position before moveUnit was aplied
+    private Position from;
+    // Final unit position
+    private Position to;
 
+    public GameEvent(Position from, Position to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    // Returns source tile
+    public Position getFrom() {
+        return from;
+    }
+
+    // Returns destination tile
+    public Position getTo() {
+        return to;
+    }
 }
