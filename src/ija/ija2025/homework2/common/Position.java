217d8d6d7f69546d000f9c36cde5e45b8b623290
Natalia Vaclavikova <xvaclan00@stud.fit.vut.cz>
2026-04-14 14:50:00 +0200

    Worked on implemetation of Game and Position

--- src/ija/ija2025/homework2/common/Position.java
+++ src/ija/ija2025/homework2/common/Position.java
@@ -1,5 +1,26 @@
 package ija.ija2025.homework2.common;
 
+import java.util.Objects;
+
 public class Position {
+    private final int row;
+    private final int col;
+
+    public Position(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public int getRow() { 
+        return row; 
+    }
     
+    public int getCol() { 
+        return col; 
+    }
+    
+    @Override
+    public String toString() { 
+        return "[" + row + "," + col + "]"; 
+    }
 }
--- src/ija/ija2025/homework2/game/Game.java
+++ src/ija/ija2025/homework2/game/Game.java
@@ -1,23 +1,47 @@
 package ija.ija2025.homework2.game;
 
+import ija.ija2025.homework2.common.Position;
+
 public class Game {
     public void addObserver(GameObserver go) {
-        
+        if(go != null) {
+            //pridat do nejake dat struktury vsech observeru
+        }
     }
 
-    public Unit createUnit(String[] str1, String[] str2, int i, int j) {
-
+    public Unit createUnit(String type, String player, int i, int j) {
+        var new_unit = new Unit(String type, String player, int i, int j, 100);
+        return new_unit;
     }
 
-    static void moveUnit(Position unitsoldposition, Position newposition) {
+    static void moveUnit(Position from, Position to) {
+        from = to;
+    }
 
+    static List<Position> getReachableTiles(Position current_position) {
+        //
     }
 }
 
 public class Unit {
     private Position position;
+    private String type;
+    private String player;
+    private int hp;
+
+    public Unit(String type, String player, int row, int col, int hp) {
+        this.position = new Position(row, col);
+        this.type = type;
+        this.player = player;
+        this.hp = hp;
+    }
 
     public Position getPosition() {
-        return position;
+        return this.position;
+    }
+
+    @Override
+    public String toString() {
+        return "{" + this.type + "[" + position.toString() + "]" + "[" + this.hp + "]}";
     }
 }
